64f1b0d6a7146e2717ee1aa5915e1a8859443896
DawidRokita <dawidrokita11@gmail.com>
2021-11-18 23:08:08 +0100

    api data

--- src/CryptoCharts.svelte
+++ src/CryptoCharts.svelte
@@ -1,5 +1,6 @@
 <script>
     let bat_rate;
+    let btc_rate;
     const api_url = "https://api.zonda.exchange/rest/trading/ticker";
     async function getData() {
         const response = await fetch(api_url);
@@ -7,9 +8,12 @@
         console.log("rate of BAT-USD is", data["items"]["BAT-USD"]["rate"]);
         console.log(data);
         bat_rate = data["items"]["BAT-USD"]["rate"];
+        btc_rate = data["items"]["BTC-USD"]["rate"];
     }
 
     getData();
 </script>
 
+<h1>bitbay api data:</h1>
 <h1>{bat_rate}</h1>
+<h1>{btc_rate}</h1>
