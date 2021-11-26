c597405ff2e794874c42e53109e20bc0988cb9ec
DawidRokita <dawidrokita11@gmail.com>
2021-11-26 19:35:54 +0100

    charts

--- src/CryptoCharts.svelte
+++ src/CryptoCharts.svelte
@@ -1,9 +1,7 @@
 <script>
   import * as myjson from "./upgrades.json";
-  import * as schema from "./schema.json";
 
   console.log(myjson);
-  console.log(schema);
 
   let bat_rate;
   let btc_rate;
@@ -15,21 +13,21 @@
     console.log(data);
     bat_rate = data["items"]["BAT-USD"]["rate"];
     btc_rate = data["items"]["BTC-USD"]["rate"];
+    console.log(Date(data["items"]["BTC-USD"]["time"]));
   }
 
   getData();
 
-  //zmienic svelte charts na chart.js - wiecej mozliwosci (lepszy dla tego programu)
   import {onMount} from 'svelte';
   function createChart() {
     const ctx = document.getElementById('myChart');
     const myChart = new Chart(ctx, {
       type: 'line',
       data: {
-          labels: [1, 2, 3, 4, 5, 6],
+          labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
           datasets: [{
               label: 'data',
-              data: [12, 19, 3, 5, 2, 3],
+              data: [12, 19, 3, 6, 2, 3, 6, 5, 34, 6, 8, 1, 5, 9],
               backgroundColor: [
                   'rgb(255,215,0)'
               ],
