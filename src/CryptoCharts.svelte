7a24506a9117294139f2d44de1cc49a7e485a4df
DawidRokita <dawidrokita11@gmail.com>
2021-11-27 10:46:53 +0100

    charts data from local json

--- src/CryptoCharts.svelte
+++ src/CryptoCharts.svelte
@@ -1,7 +1,6 @@
 <script>
-  import * as myjson from "./upgrades.json";
-
-  console.log(myjson);
+  import * as myjson from "./test.json";
+  console.log(myjson.default["1"]["time"]);
 
   let bat_rate;
   let btc_rate;
@@ -9,56 +8,56 @@
   async function getData() {
     const response = await fetch(api_url);
     const data = await response.json();
-    console.log("rate of BAT-USD is", data["items"]["BAT-USD"]["rate"]);
-    console.log(data);
     bat_rate = data["items"]["BAT-USD"]["rate"];
     btc_rate = data["items"]["BTC-USD"]["rate"];
-    console.log(Date(data["items"]["BTC-USD"]["time"]));
   }
 
   getData();
 
-  import {onMount} from 'svelte';
-  function createChart() {
-    const ctx = document.getElementById('myChart');
-    const myChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-          labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
-          datasets: [{
-              label: 'data',
-              data: [12, 19, 3, 6, 2, 3, 6, 5, 34, 6, 8, 1, 5, 9],
-              backgroundColor: [
-                  'rgb(255,215,0)'
-              ],
-              borderColor: [
-                  'rgb(255,215,0)'
-              ],
-              borderWidth: 2
-          }]
-      },
-      options: {
-          scales: {
-              y: {
-                  beginAtZero: true
-              }
-          }
-      }
-  });
-}
-onMount(createChart);
+  const xlabels = []; //x data chart
+  const ratedata = []; //y data charts
 
+  let arr = Object.entries(myjson.default); //json data to array
+  for (let i = 0; i < arr.length; i++) {
+    xlabels[i] = arr[i][1].time;
+    ratedata[i] = arr[i][1].rate;
+  }
+
+  import { onMount } from "svelte";
+  async function createChart() {
+    await getData();
+    const ctx = document.getElementById("myChart");
+
+    const myChart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: xlabels,
+        datasets: [
+          {
+            label: "data",
+            data: ratedata,
+            backgroundColor: "rgb(255,215,0)",
+            borderColor: "rgb(255,215,0)",
+            borderWidth: 2,
+            pointRadius: 0,
+          },
+        ],
+      },
+    });
+  }
+  onMount(createChart);
 </script>
 
 <h1>bitbay api data:</h1>
 <h1>BAT: {bat_rate}</h1>
 <h1>BTC: {btc_rate}</h1>
-<canvas id="myChart"></canvas>
+<canvas id="myChart" />
 
 <style>
-  #myChart{
-    background: white;
+  #myChart {
+    background: rgb(146, 146, 146);
     width: 100%;
     height: 100;
+    margin: 20px;
   }
 </style>
